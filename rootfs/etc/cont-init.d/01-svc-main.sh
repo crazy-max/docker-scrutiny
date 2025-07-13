d4930fd917373bf7032f1c0fd55217e2f4b5a6ae
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-07-13 02:23:40 +0200

    run collector at startup as service

--- rootfs/etc/cont-init.d/01-svc-main.sh
+++ rootfs/etc/cont-init.d/01-svc-main.sh
@@ -12,9 +12,6 @@ echo "InfluxDB is ready!"
 
 echo "S.M.A.R.T version: $(apk list --installed smartmontools | cut -d' ' -f1 | cut -d'-' -f2-)"
 
-echo "Running Scrutiny collector..."
-scrutiny-collector-metrics run
-
 mkdir -p /etc/services.d/scrutiny
 cat > /etc/services.d/scrutiny/run <<EOL
 #!/usr/bin/execlineb -P
@@ -22,3 +19,27 @@ with-contenv
 scrutiny start
 EOL
 chmod +x /etc/services.d/scrutiny/run
+
+mkdir -p /etc/services.d/scrutiny-collector-oneshot
+cat > /etc/services.d/scrutiny-collector-oneshot/run <<EOL
+#!/usr/bin/with-contenv bash
+
+if [ -f /tmp/.scrutiny-collector-oneshot-done ]; then
+  s6-svc -d /run/s6/services/scrutiny-collector-oneshot
+  exit 0
+fi
+
+echo "Waiting for scrutiny service to start..."
+s6-svwait -u /run/s6/services/scrutiny
+until curl --output /dev/null --silent --head --fail "http://localhost:8080/api/health"; do
+  sleep 2
+done
+
+echo "Running scrutiny collector on startup..."
+scrutiny-collector-metrics run
+
+touch /tmp/.scrutiny-collector-oneshot-done
+s6-svc -d /run/s6/services/scrutiny-collector-oneshot
+exit 0
+EOL
+chmod +x /etc/services.d/scrutiny-collector-oneshot/run
